af17ec5f0968692380c31a3792847c2528bfc3a3
Mike Hunt <100328348+That411Guy@users.noreply.github.com>
2023-07-14 15:58:18 -0400

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "Path Of Titans Egg by Marcel Baumgartner"
+echo "(c) Copyright 2022 endelon-hosting.de"
+echo "Checking for updates"
+export INSTALL_DIR="./"
+./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir .
+echo "Starting server"
+UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
+chmod +x "$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping"
+"$UE_PROJECT_ROOT/PathOfTitans/Binaries/Linux/PathOfTitansServer-Linux-Shipping" PathOfTitans -log -Port=$SERVER_PORT -BranchKey=$BETA_BRANCH -AuthToken=$AG_AUTH_TOKEN -ServerGUID=$SERVER_GUID -Database=$DATABASE
+# For getting the server list to work you need to add -ServerListIP=000.000.000 option
