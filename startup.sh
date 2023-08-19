5650c2c248559709bcc676136832d93563cf5f16
Mike Hunt <100328348+That411Guy@users.noreply.github.com>
2023-08-18 21:04:57 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,6 @@ echo "Path Of Titans Egg by GameServersHub"
 echo "LLC 2017 GameServersHub.com"
 echo "Checking for updates"
 export INSTALL_DIR="./"
-export DOTNET_BUNDLE_EXTRACT_BASE_DIR=./temp/
 ./AlderonGamesCmd --game path-of-titans --server true --beta-branch $BETA_BRANCH --install-dir .
 echo "Starting server"
 UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
